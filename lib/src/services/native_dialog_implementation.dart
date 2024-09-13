221bef95c706abe788c29a723767041900041f77
enricoroselino <enricoroselino@gmail.com>
2024-09-13 10:22:13 +0700

    added information alert

--- lib/src/services/native_dialog_implementation.dart
+++ lib/src/services/native_dialog_implementation.dart
@@ -2,22 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter_platform_alert/flutter_platform_alert.dart';
 
 abstract interface class IDialog {
-  Future<bool> confirmation(
-    String title,
-    String content, {
-    BuildContext? context,
+  Future<bool> confirmation({
+    required BuildContext context,
+    required String title,
+    required String content,
+  });
+
+  Future<void> info({
+    required BuildContext context,
+    required String title,
+    required String content,
   });
 }
 
 class NativeDialogImplementation implements IDialog {
   @override
-  Future<bool> confirmation(
-    String title,
-    String content, {
-    BuildContext? context,
+  Future<bool> confirmation({
+    required BuildContext context,
+    required String title,
+    required String content,
   }) async {
-    if (context != null && !context.mounted) return false;
-
     final clickedButton = await FlutterPlatformAlert.showAlert(
       windowTitle: title,
       text: content,
@@ -26,5 +30,14 @@ class NativeDialogImplementation implements IDialog {
     );
 
     return clickedButton == AlertButton.noButton ? false : true;
+  }
+
+  @override
+  Future<void> info({
+    required BuildContext context,
+    required String title,
+    required String content,
+  }) async {
+    FlutterPlatformAlert.showAlert(windowTitle: title, text: content);
   }
 }
