dc5c975f33dead50a8f0a78909dafb1690bf312a
enricoroselino <enricoroselino@gmail.com>
2024-09-13 10:28:19 +0700

    make interface, hide constructor

--- lib/src/services/debounce.dart
+++ lib/src/services/debounce.dart
@@ -1,30 +1,38 @@
 import 'dart:async';
 import 'dart:ui';
 
-class Debounce {
+abstract interface class IDebounce {
+  void run(VoidCallback action);
+
+  void dispose();
+}
+
+class Debounce implements IDebounce {
   late final Duration _delay;
   Timer? _timer;
 
-  Debounce({required Duration delay}) {
+  Debounce._({required Duration delay}) {
     _delay = delay;
   }
 
+  @override
   void run(VoidCallback action) {
     _timer?.cancel();
     _timer = Timer(_delay, action);
   }
 
+  @override
   void dispose() {
     _timer?.cancel();
     _timer = null;
   }
 
-  static Debounce get debounce250ms =>
-      Debounce(delay: const Duration(milliseconds: 250));
+  static IDebounce get debounce250ms =>
+      Debounce._(delay: const Duration(milliseconds: 250));
 
-  static Debounce get debounce500ms =>
-      Debounce(delay: const Duration(milliseconds: 500));
+  static IDebounce get debounce500ms =>
+      Debounce._(delay: const Duration(milliseconds: 500));
 
-  static Debounce get debounce750ms =>
-      Debounce(delay: const Duration(milliseconds: 750));
+  static IDebounce get debounce750ms =>
+      Debounce._(delay: const Duration(milliseconds: 750));
 }
